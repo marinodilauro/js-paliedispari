976177349739c9169cf9650572ad93e361b1060a
marinodilauro <m.dilauro90@gmail.com>
2024-02-27 14:12:41 +0100

    Add varaible for user input. Turn string variable into an array

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// PALINDROMOUS WORD
+// Ask the user to enter a word and create a function to check if the entered word is a palindrome
+
+// Create a varaible and assign the word entered by the user
+
+let userWord = prompt("Inserire una parola");
+console.log(userWord);
+
+
+//  Turn the string into an array
+
+userWord = Array.from(userWord)
+console.log(userWord);
+
