6c07162dbd570b66809d4ca02ebc842d8feae525
marinodilauro <m.dilauro90@gmail.com>
2024-02-27 14:36:20 +0100

    Refactor code in a function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,42 @@
 // PALINDROMOUS WORD
 // Ask the user to enter a word and create a function to check if the entered word is a palindrome
 
-// Create a varaible and assign the word entered by the user
 
-const userWord = prompt("Inserire una parola");
-console.log(userWord);
+function isPalindrome(userWord) {
 
+  // Create a varaible and assign the word entered by the user
 
-//  Turn the string into an array
-
-const userWordArray = Array.from(userWord)
-console.log(userWord);
+  userWord = prompt("Inserire una parola");
 
 
-// Reverse order of the letters
+  //  Turn the string into an array
 
-const userWordArrayReversed = userWordArray.reverse();
-console.log(userWordArrayReversed);
-
-
-//  Turn the array into a string
-
-const userWordReversed = userWordArrayReversed.join("");
-console.log(userWordReversed);
+  const userWordArray = Array.from(userWord)
+  //console.log(userWord);
 
 
-// Check if the word is palindrome
+  // Reverse order of the letters
 
-if (userWord === userWordReversed) {
-  alert("La parola è palindroma");
-} else {
-  alert("La parola non è palindroma");
-};
+  const userWordArrayReversed = userWordArray.reverse();
+  //console.log(userWordArrayReversed);
+
+
+  //  Turn the array into a string
+
+  const userWordReversed = userWordArrayReversed.join("");
+  //console.log(userWordReversed);
+
+
+  // Check if the word is palindrome
+
+  if (userWord === userWordReversed) {
+    return "è palindroma"
+  } else {
+    return "non è palindroma"
+  };
+
+}
+
+checkPalindromous = isPalindrome();
+
+alert(`La parola ${checkPalindromous}`);
