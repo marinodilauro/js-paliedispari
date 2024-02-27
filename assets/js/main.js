5585ae41ba73d702a85b1ffd77e5e9c797ae34f5
marinodilauro <m.dilauro90@gmail.com>
2024-02-27 14:24:02 +0100

    Reverse the order of letters and turn the array into a string

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,32 @@
 
 // Create a varaible and assign the word entered by the user
 
-let userWord = prompt("Inserire una parola");
+const userWord = prompt("Inserire una parola");
 console.log(userWord);
 
 
 //  Turn the string into an array
 
-userWord = Array.from(userWord)
+const userWordArray = Array.from(userWord)
 console.log(userWord);
 
+
+// Reverse order of the letters
+
+const userWordArrayReversed = userWordArray.reverse();
+console.log(userWordArrayReversed);
+
+
+//  Turn the array into a string
+
+const userWordReversed = userWordArrayReversed.join("");
+console.log(userWordReversed);
+
+
+// Check if the word is palindrome
+
+if (userWord === userWordReversed) {
+  alert("La parola è palindroma");
+} else {
+  alert("La parola non è palindroma");
+};
